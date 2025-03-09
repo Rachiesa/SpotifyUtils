46a1e60d6ad63091d04bc7634cefed952feec32e
Rachiesa Yazid Audi <audichiesa99@gmail.com>
2025-03-09 23:27:31 +0700

    fix
    fix

--- src/app/utils/Utils.jsx
+++ src/app/utils/Utils.jsx
@@ -2,7 +2,7 @@
 export default function Utils() {
     return (
         <main className="flex flex-col w-full h-full font-anton gap-3 text-3xl">
-            <a href="utils/.song">RANDOM.SONG</a>
+            <a href="utils/song">RANDOM.SONG</a>
             <a href=".genre">RANDOM.GENRE.ONWORKS</a>
             <a href=".artist">RANDOM.ARTIST.ONWORKS</a>
             <a href=".album">RANDOM.ALBUM.ONWORKS</a>

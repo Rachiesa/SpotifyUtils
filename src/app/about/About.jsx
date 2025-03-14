d82fb6ec342792d4adb141693ad5f602dc86f656
Rachiesa Yazid Audi <audichiesa99@gmail.com>
2025-03-14 08:58:39 +0700

    Update About.jsx

--- src/app/about/About.jsx
+++ src/app/about/About.jsx
@@ -1,13 +1,14 @@
-
-
+"use client";
 export default function About() {
     return (
-        <main>
-            <p className="font-trispace text-lg md:text-2xl w-3/5">HELLO, RACHIESA HERE!
-                THIS IS MY FIRST PROJECT,<br /> I LOVE LISTENING MUSIC WITH SPOTIFY.
-                MY GOALS IS
+        <main className="flex flex-col items-start justify-start w-full h-full">
+            <p className="font-trispace text-xl md:text-2xl w-full">
+                HELLO, RACHIESA HERE!
+                THIS IS MY FIRST PROJECT, I LOVE LISTENING MUSIC WITH SPOTIFY.
+                <br /> MY GOALS IS
                 TO USE SPOTIFY API FOR EXPLORING NEW MUSIC. PLEASE ENJOY AND REPORT
                 IF SOMETHING WENT WRONG.</p>
+            <br />
             <h2 className="underline font-anton text-3xl">THANKS!!!</h2>
         </main>
     )
